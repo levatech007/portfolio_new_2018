ad2cbaee7fa75e6934663f884fccf1f62a64deba
Eva <evaliisagalen@gmail.com>
2018-09-03 16:42:28 +0300

    contact form styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -88,62 +88,59 @@ class Contact extends Component {
 
   render() {
     return(
-      <div className="row justify-content-center">
-              <form onSubmit={ this.onFormSubmit } className="forms">
-                  <div className="form-group row">
-                    <label className="col-sm-4 col-form-label">Name:</label>
-                    <div className="col-sm-8">
-                      <input
-                        type="text"
-                        className="form-control"
-                        placeholder="Your name"
-                        onChange={this.onNameInputChange}
-                      />
-                    </div>
-                  </div>
-                  <br></br>
-                  <div className="form-group row">
-                    <label className="col-sm-4 col-form-label">Email:</label>
-                    <div className="col-sm-8">
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Your email"
-                        onChange={this.onEmailInputChange}
-                      />
-                    </div>
-                  </div>
-                  <br></br>
-                  <div className="form-group row">
-                    <label className="col-sm-4 col-form-label">Notes:</label>
-                    <div className="col-sm-8">
-                      <textarea
-                        rows="4"
-                        placeholder="Your message here"
-                        onChange={this.onNoteInputChange}
-                        >
-                        </textarea>
-                    </div>
-                  </div>
-                  <div className="row justify-content-center">
-                    <div className="col-md-8 offset-md-4">
-                    <ReCAPTCHA
-                      ref="recaptcha"
-                      sitekey="6LcEkV0UAAAAAESH0bsrhEvys2MIK2aE3yhWeHTi"
-                      onChange={ this.onChange }/>
-                    </div>
-                  </div>
-                  <div className="row justify-content-center submit-btn">
-                    <div className="col-md-8 offset-md-4">
-                      <input
-                        type="submit"
-                        className="btn btn-primary"
-                        value="Submit your request"
-                      />
-                    </div>
-                  </div>
-                </form>
+      <div className="row justify-content-center main-content">
+        <div className="col-md-6">
+          <form onSubmit={ this.onFormSubmit } className="forms">
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Name:</label>
+              <div className="col-sm-8">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Your name"
+                  onChange={this.onNameInputChange}
+                />
               </div>
+            </div>
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Email:</label>
+              <div className="col-sm-8">
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Your email"
+                  onChange={this.onEmailInputChange}
+                />
+              </div>
+            </div>
+            <div className="form-group row">
+              <label className="col-sm-2 col-form-label">Notes:</label>
+              <div className="col-sm-8">
+                <textarea
+                  rows="4"
+                  className="form-control"
+                  placeholder="Your message here"
+                  onChange={this.onNoteInputChange}
+                />
+              </div>
+            </div>
+            <div className="row justify-content-center">
+              <ReCAPTCHA
+                ref="recaptcha"
+                sitekey="6LcEkV0UAAAAAESH0bsrhEvys2MIK2aE3yhWeHTi"
+                onChange={ this.onChange }
+              />
+            </div>
+            <div className="row justify-content-center">
+              <input
+                type="submit"
+                className="btn btn-dark submit-btn"
+                value="Submit"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
     )
   }
 }
